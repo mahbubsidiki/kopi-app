609c9582ac09ebaea03c584b1ac1be913c2138b8
mahbubsidiki <mahbubsidiki1@gmail.com>
2026-06-19 16:08:04 +0700

    Hubungkan footer langsung ke konten dan tambahkan cache-busting CSS

--- includes/header.php
+++ includes/header.php
@@ -18,7 +18,7 @@ $current_page = basename($_SERVER['PHP_SELF']);
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400;0,600;0,700;1,400&family=DM+Sans:wght@300;400;500&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="/kopi-app/assets/css/style.css">
+    <link rel="stylesheet" href="/kopi-app/assets/css/style.css?v=<?= filemtime(__DIR__ . '/../assets/css/style.css') ?>">
 </head>
 <body>
 
